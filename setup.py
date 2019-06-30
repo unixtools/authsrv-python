a32f9e000c5cb022e8536f73090f5ed0e151374f
Nathan Neulinger <nneul@neulinger.org>
2019-06-29 19:22:16 -0500

    base import

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='authsrv',
+    version='0.0.1',
+    description='wrapper for authsrv utilities',
+    license='MIT',
+    packages=['authsrv'],
+    author='Nathan Neulinger',
+    author_email='nneul@neulinger.org',
+    keywords=['authsrv'],
+    url='https://github.com/unixtools/authsrv-python'
+)
+
