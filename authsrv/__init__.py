dd1b96b5940a109bb641ac8ebffc33a8afcabc43
Nathan Neulinger <nneul@neulinger.org>
2022-03-20 17:02:56 -0500

    better error handling and behavior

--- authsrv/__init__.py
+++ authsrv/__init__.py
@@ -3,21 +3,29 @@ Wrapper around authsrv utilities from https://github.com/unixtools/authsrv
 '''
 
 import subprocess
+from subprocess import PIPE
+
 import getpass
 
 __author__ = 'Nathan Neulinger'
 __url__ = 'https://github.com/unixtools/authsrv-python'
-__version__ = '0.0.6'
+__version__ = '0.0.8'
 
 
-def fetch(owner: str = None, user='', instance=''):
+def fetch(owner: str = None, user='', instance='') -> str:
     '''Retrieve a stash from authsrv'''
 
     if owner is None:
         owner = getpass.getuser()
 
-    password = subprocess.check_output(["authsrv-decrypt", owner, user, instance])
-    password = password.decode('utf-8')
-    password = password.rstrip('\n\r')
+    if user = '' or instance = '':
+        return None
+
+    try:
+        password = subprocess.check_output(["authsrv-decrypt", owner, user, instance], stderr=PIPE)
+        password = password.decode('utf-8')
+        password = password.rstrip('\n\r')
+    except subprocess.CalledProcessError:
+        password = None
 
     return password
